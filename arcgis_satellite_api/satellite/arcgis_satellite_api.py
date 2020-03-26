93395e6add793a853f55fa67ed4430088ce134d1
Berry <berry@goedgemerkt.nl>
2020-03-26 18:19:02 +0100

    Added docstrings

--- arcgis_satellite_api/satellite/arcgis_satellite_api.py
+++ arcgis_satellite_api/satellite/arcgis_satellite_api.py
@@ -3,12 +3,26 @@ import os
 from urllib.request import urlopen
 from matplotlib.pyplot import imread, imsave
 
-
 class Satellite_data():
+    """ Class to reach the Arcgis World Imagery Web API."""
+
     def __init__(self):
+        """ Constructor method, registers the not found image + average
+        """                
         self.notfound = imread("data/not_found.jpeg")
         self.notfound_avg = self.notfound.mean(axis=0).mean(axis=0)[0:3]
-    def download_tile(self, lat, lon):    
+    
+    def download_tile(self, lat, lon):
+        """ Download a tile from the Arcgis World Imagery API.
+        
+        :param lat: Latitude value from desired tile.
+        :type lat: float
+        :param lon: Longitude value from desired tile.
+        :type lon: float
+        :return: Dictionary with filename of downloaded image, top left & right bottom bound in (lat,lon) values and zoom level.
+        :rtype: dict
+        """        
+
         if not os.path.isdir("data/images"):
             os.mkdir('data/images')
 
@@ -18,26 +32,41 @@ class Satellite_data():
         local_file = open(filename, 'wb')
         imsave(local_file, image)
 
-        bounds = self.get_bounds(tile_x, tile_y, zoom)
+        bound_top_left, bound_bottom_right = self.get_bounds(tile_x, tile_y, zoom)
         return {
             'filename': filename,
             'bounds': {
-                'top_left': bounds[0],
-                'bottom_right': bounds[1]
+                'top_left': bound_top_left,
+                'bottom_right': bound_bottom_right,
             },
             'zoom': zoom
         }
     	
     def get_zoom_level_image(self, lat, lon, zoom):
+        """ Get a tile from the Arcgis World Imagery API with desired zoomlevel.
+        
+        :param lat: Latitude value from desired tile.
+        :type lat: float
+        :param lon: Longitude value from desired tile.
+        :type lon: float
+        :return: Desired image as Numpy Array, tile x-value, tile y-value, zoomlevel of retrieved image.
+        :rtype: numpy.ndarray, tuple, tuple, int
+
+        .. note::
+            When an image on the desired zoom level is not available, there will be searched for a lower resolution image. Desired zoomlevel is not guaranteed.
+
+        """   
+
         if zoom < 0 or zoom > 23:
             raise Exception("Zoom can't be lower than 0 or higher than 23.")
+
         tile_y = self.lat_to_tile_Y(lat, zoom)
         tile_x = self.long_to_tile_X(lon, zoom)
 
         image_url = f"https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{zoom}/{tile_y}/{tile_x}"
         with urlopen(image_url) as image_file:
             image = imread(image_file, "jpeg")
-            
+
             img_average = image.mean(axis=0).mean(axis=0)[0:3]
             if all(img_average == self.notfound_avg):
                 return self.get_zoom_level_image(lat, lon, zoom - 1)
@@ -45,12 +74,41 @@ class Satellite_data():
             return (image, tile_x, tile_y, zoom)
 
     def long_to_tile_X(self, lon, zoom):
+        """Calculate tile x-value based on longitude and zoom value
+        
+        :param lon: Longitude value.
+        :type lon: float
+        :param zoom: Zoom level.
+        :type zoom: int
+        :return: Tile x-value 
+        :rtype: int
+        """        
         return math.floor((lon+180) / 360*math.pow(2, zoom))
 
     def lat_to_tile_Y(self, lat, zoom):
+        """Calculate tile y-value based on latitude and zoom value
+        
+        :param lon: Latitude value.
+        :type lon: float
+        :param zoom: Zoom level.
+        :type zoom: int
+        :return: Tile y-value 
+        :rtype: int
+        """   
         return (math.floor((1-math.log(math.tan(lat*math.pi/180) + 1/math.cos(lat*math.pi/180))/math.pi)/2 * math.pow(2, zoom)))
 
-    def num2deg(self, tile_x, tile_y, zoom):
+    def get_top_left_bound(self, tile_x, tile_y, zoom):
+        """ Get top left boud of image.
+        
+        :param tile_x: Tile x value. Can be calculated with `long_to_tile_X`.
+        :type tile_x: int
+        :param tile_y: Tile y value. Can be calculated with `lat_to_tile_Y`.
+        :type tile_y: int
+        :param zoom: Zoom level of image.
+        :type zoom: int
+        :return: Top left bound (lat, lon) values
+        :rtype: float
+        """
         n = 2.0 ** zoom
         lon_deg = tile_x / n * 360.0 - 180.0
         lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * tile_y / n)))
@@ -58,12 +116,17 @@ class Satellite_data():
         return (lat_deg, lon_deg)
 
     def get_bounds(self, tile_x, tile_y, zoom):
-        top_left = self.num2deg(tile_x, tile_y, zoom)
-        right_bottom = self.num2deg(tile_x + 1, tile_y + 1, zoom)
-        return (top_left, right_bottom)
-
-
-if __name__ == "__main__":
-    sd = Satellite_data()
-    returnval = sd.download_tile(30, 23)
-    print(returnval)
+        """Get bounds of image with defined tile x-value and y-value
+        
+        :param tile_x: Tile x value. Can be calculated with `long_to_tile_X`.
+        :type tile_x: int
+        :param tile_y: Tile y value. Can be calculated with `lat_to_tile_Y`.
+        :type tile_y: int
+        :param zoom: Zoom level of image.
+        :type zoom: int
+        :return: Top left bound (lat, lon) values, bottom right (lat, lon) values.
+        :rtype: float, float
+        """        
+        top_left = self.get_top_left_bound(tile_x, tile_y, zoom)
+        bottom_right = self.get_top_left_bound(tile_x + 1, tile_y + 1, zoom)
+        return (top_left, bottom_right)
