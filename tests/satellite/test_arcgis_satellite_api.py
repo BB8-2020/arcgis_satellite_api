2ff15c161d30d515b7e97bc3c48642601226f562
Berry <berry@goedgemerkt.nl>
2020-03-27 12:15:59 +0100

    Added initial test

--- tests/satellite/test_arcgis_satellite_api.py
+++ tests/satellite/test_arcgis_satellite_api.py
@@ -1,7 +1,22 @@
-from arcgis_satellite_api.satellite import arcgis_satellite_api as sat
-import unittest
+import os
+import sys 
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+root_dir = os.path.dirname(parentdir)
+sys.path.insert(0,root_dir) 
 
+import unittest
+from arcgis_satellite_api.satellite import arcgis_satellite_api as sd
+
+
+sat = sd.Satellite_data()
 
 class TestArcgisAPI(unittest.TestCase):
-    def test__google(self):
-        assert sat.image is not None
+    def test__init(self):
+        assert sat.notfound is not None
+        assert sat.notfound_avg is not None
+        assert len(sat.notfound_avg) == 3
+
+if __name__ == '__main__':
+    unittest.main()
