31a9fa26868b276330cd1d7b93e8da59669f13f2
Berry <berry@goedgemerkt.nl>
2020-06-09 11:57:48 +0200

    [HIT-154] Extend ArcGis API with height maps

--- arcgis_satellite_api/satellite/arcgis_satellite_api.py
+++ arcgis_satellite_api/satellite/arcgis_satellite_api.py
@@ -11,19 +11,30 @@ class Satellite_data():
     def __init__(self):
         """ Constructor method, registers the not found image + average
         """
+        self.base_url = "https://server.arcgisonline.com/arcgis/rest/services"
+        self.tile_maps = {
+            'world_imagery': 'World_Imagery',
+            'world_hillshade': 'Elevation/World_Hillshade',
+            'world_hillshade_dark': 'Elevation/World_Hillshade_Dark'
+        }
         self.data_folder = '/'.join(__file__.replace('\\',
                                                      '/').split('/')[:-1]) + "/data/"
         self.notfound = imread(self.data_folder + "not_found.jpeg")
         self.notfound_avg = self.notfound.mean(axis=0).mean(axis=0)[0:3]
 
-    def download_tile(self, lat, lon, zoom=23):
+    def download_tile(self, lat, lon, zoom=23, map_type="world_imagery"):
         """ Download a tile from the Arcgis World Imagery API.
 
         :param lat: Latitude value from desired tile.
         :type lat: float
         :param lon: Longitude value from desired tile.
         :type lon: float
-        :return: Dictionary with filename of downloaded image, top left & right bottom bound in (lat,lon) values, pixel where given coordinate is located (x,y) and zoom level.
+        :param zoom: Zoom level of desired tile.
+        :type zoom: int
+        :param map_type: Desired map type, currently supports: world_imagery, world_hillshade, world_hillshade_dark. (http://server.arcgisonline.com/arcgis/rest/services/)
+        :type map_type: str
+        :return: Dictionary with filename of downloaded image,
+            top left & right bottom bound in (lat,lon) values, pixel where given coordinate is located (x,y) and zoom level.
         :rtype: dict
         """
 
@@ -31,17 +42,17 @@ class Satellite_data():
             os.mkdir(f"{self.data_folder}images")
 
         image, tile_x, tile_y, zoom = self.get_zoom_level_image(
-            lat, lon, zoom=23)
+            lat, lon, zoom=23, map_type=map_type)
 
-        filename = f"{self.data_folder}images/{tile_x}_{tile_y}_{zoom}.jpeg"
+        filename = f"{self.data_folder}images/{map_type}_{tile_x}_{tile_y}_{zoom}.jpeg"
         local_file = open(filename, 'wb')
         imsave(local_file, image)
 
         bound_top_left, bound_bottom_right = self.get_bounds(
             tile_x, tile_y, zoom)
-        coordinate_pixel_x, coordinate_pixel_y = self.get_pixel_location((lat,lon,),
-            bound_top_left, bound_bottom_right)
-        
+        coordinate_pixel_x, coordinate_pixel_y = self.get_pixel_location((lat, lon,),
+                                                                         bound_top_left, bound_bottom_right)
+
         return {
             'filename': filename,
             'bounds': {
@@ -55,18 +66,24 @@ class Satellite_data():
             'zoom': zoom
         }
 
-    def get_zoom_level_image(self, lat, lon, zoom):
+    def get_zoom_level_image(self, lat, lon, zoom, map_type="world_imagery"):
         """ Get a tile from the Arcgis World Imagery API with desired zoomlevel.
 
         :param lat: Latitude value from desired tile.
         :type lat: float
         :param lon: Longitude value from desired tile.
         :type lon: float
-        :return: Desired image as Numpy Array, tile x-value, tile y-value, zoomlevel of retrieved image.
+        :param zoom: Zoom level of desired tile.
+        :type zoom: int
+        :param map_type: Desired map type, currently supports: world_imagery, world_hillshade, world_hillshade_dark. (http://server.arcgisonline.com/arcgis/rest/services/)
+        :type map_type: str
+        :return: Desired image as Numpy Array, tile x-value, tile y-value,
+            zoomlevel of retrieved image.
         :rtype: numpy.ndarray, tuple, tuple, int
 
         .. note::
-            When an image on the desired zoom level is not available, there will be searched for a lower resolution image. Desired zoomlevel is not guaranteed.
+            When an image on the desired zoom level is not available,
+                there will be searched for a lower resolution image. Desired zoomlevel is not guaranteed.
 
         """
 
@@ -76,13 +93,14 @@ class Satellite_data():
         tile_y = self.lat_to_tile_Y(lat, zoom)
         tile_x = self.long_to_tile_X(lon, zoom)
 
-        image_url = f"https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{zoom}/{tile_y}/{tile_x}"
+        image_url = f"{self.base_url}/{self.tile_maps[map_type]}/MapServer/tile/{zoom}/{tile_y}/{tile_x}"
+
         with urlopen(image_url) as image_file:
             image = imread(image_file, "jpeg")
 
             img_average = image.mean(axis=0).mean(axis=0)[0:3]
             if all(img_average == self.notfound_avg):
-                return self.get_zoom_level_image(lat, lon, zoom - 1)
+                return self.get_zoom_level_image(lat, lon, zoom - 1, map_type)
 
             return (image, tile_x, tile_y, zoom)
 
@@ -93,7 +111,7 @@ class Satellite_data():
         :type lon: float
         :param zoom: Zoom level.
         :type zoom: int
-        :return: Tile x-value 
+        :return: Tile x-value
         :rtype: int
         """
         return math.floor((lon+180) / 360*math.pow(2, zoom))
@@ -105,10 +123,10 @@ class Satellite_data():
         :type lon: float
         :param zoom: Zoom level.
         :type zoom: int
-        :return: Tile y-value 
+        :return: Tile y-value
         :rtype: int
         """
-        return (math.floor((1-math.log(math.tan(lat*math.pi/180) + 1/math.cos(lat*math.pi/180))/math.pi)/2 * math.pow(2, zoom)))
+        return math.floor((1-math.log(math.tan(lat*math.pi/180) + 1/math.cos(lat*math.pi/180))/math.pi)/2 * math.pow(2, zoom))
 
     def get_top_left_bound(self, tile_x, tile_y, zoom):
         """ Get top left boud of image.
@@ -146,7 +164,7 @@ class Satellite_data():
 
     def get_pixel_location(self, loc, bound_top_left, bound_bottom_right, img_size=256):
         """ Get pixel location of given  (lat,lon) location in given bounds
-        
+
         :param loc: Latitude and Longitude to get pixel location for.
         :type loc: tuple
         :param bound_top_left: Top left bound, latitude and longitude
@@ -157,7 +175,7 @@ class Satellite_data():
         :type img_size: int, optional
         :return: Location in pixels of given coordinate
         :rtype: int, int
-        """        
+        """
         x_top, y_left = bound_top_left
         x_bottom, y_right = bound_bottom_right
 
--- examples/download_tiles.py
+++ examples/download_tiles.py
@@ -1,13 +1,16 @@
 import os
-import sys 
-import inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-
+import sys
+EXAMPLES_FOLDER = os.path.abspath(os.path.join(__file__, os.pardir))
+PROJECT_ROOT = os.path.abspath(os.path.join(EXAMPLES_FOLDER, os.pardir))
+sys.path.append(PROJECT_ROOT)
 from arcgis_satellite_api.satellite import arcgis_satellite_api as sat
 
-sd = sat.Satellite_data()
-data = sd.download_tile(52.084323, 5.175941)
 
-print(data)
+sd = sat.Satellite_data()
+data1 = sd.download_tile(-0.608854, 34.205806)
+data2 = sd.download_tile(-0.608854, 34.205806, map_type="world_hillshade")
+data3 = sd.download_tile(-0.608854, 34.205806, map_type="world_hillshade_dark")
+
+print(data1)
+print(data2)
+print(data3)
