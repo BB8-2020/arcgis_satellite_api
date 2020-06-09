bcc1bcfa9588fcfda9c12c4d9366e178248c609b
Berry <berry@goedgemerkt.nl>
2020-06-09 14:56:23 +0200

    Added zoom param to example

--- examples/download_tiles.py
+++ examples/download_tiles.py
@@ -8,7 +8,7 @@ from arcgis_satellite_api.satellite import arcgis_satellite_api as sat
 
 
 sd = sat.Satellite_data()
-data1 = sd.download_tile(-0.608854, 34.205806)
+data1 = sd.download_tile(-0.608854, 34.205806, zoom=13)
 data2 = sd.download_tile(-0.608854, 34.205806, map_type="world_hillshade")
 data3 = sd.download_tile(-0.608854, 34.205806, map_type="world_hillshade_dark")
 
